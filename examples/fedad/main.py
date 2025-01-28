a6dd17c8192210a0c0c2fa27da3f81a14488e25b
Pieli <49155289+Pieli@users.noreply.github.com>
2025-01-28 16:10:05 +0100

    Changed some defaults

--- examples/fedad/main.py
+++ examples/fedad/main.py
@@ -6,13 +6,14 @@ parser = ArgumentParser()
 
 parser.add_argument("-e", "--epochs", type=int, default=3)
 parser.add_argument("-k", "--nodes", type=int, default=1)
-parser.add_argument("-lb", "--local_batch", type=int, default=128)
+parser.add_argument("-lb", "--local_batch", type=int, default=10)
 parser.add_argument("-t", "--rounds", type=int, default=128)
 
 
 parser.add_argument("-w", "--workers", type=int, default=4)
 
-parser.add_argument("--dev-batches", type=int, default=4)
+parser.add_argument("--dev-batches", type=int, default=None)
+parser.add_argument("--seed", type=int, default=42)
 
 
 args = parser.parse_args()
